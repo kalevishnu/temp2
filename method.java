c3ab42fb6f5179bc25c5b5faa75d196e81939be4
Vishnu kale <kalevishnu759@gmail.com>
2024-02-15 10:26:32 +0530

    added

---
+++ method.java
@@ -0,0 +1,23 @@
+
+public class method {
+    public static void main(String[] args) {
+        addMethod(); mulMethod();
+    }
+    public static void addMethod(){
+        int i =10;
+        int j =20;
+        int sum=i+j;
+        System.out.println(sum);
+    }
+    public static void mulMethod (){
+        int j=20;
+        int k =20;
+        int mul=j*k;
+        System.out.println(mul);
+    }
+    
+        
+    }
+
+
+    
